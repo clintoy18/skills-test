2ad4ef7fd1580ee03847f2c3221de238fb330bac
clintoy18 <aicsalonzo@gmail.com>
2025-06-03 21:36:19 +0800

    update

--- Classes/Database.php
+++ Classes/Database.php
@@ -1,26 +1,23 @@
 <?php
 
-  class Database{
-    private $hostname = "localhost";
+class Database{
+
+    private $host = "localhost";
     private $username = "root";
     private $password = "";
     private $dbname = "studentmanagement";
+    public $connection; // variable to store the connection
 
-    public $connection;
+    //function to connect to the database
+    public function connect(){
+      $this->connection = new mysqli($this->host,$this->username,$this->password,$this->dbname);  
 
-   public function connect(){
-      $this->connection = new mysqli($this->hostname,$this->username, $this->password, $this->dbname);
       if($this->connection->connect_error){
-         die("Connection Error". $this->connection->connect_error);
+        die("Connection Error".$this->connection->connect_error );
       }
       return $this->connection;
+    }
 
-   }
-
-
-
-  }
-
-
+}
 
 ?>
--- Classes/Student.php
+++ Classes/Student.php
@@ -1,43 +1,49 @@
 <?php
- require_once "classes/Database.php";
 
+require_once "classes/Database.php";
 
- class Student{
-    
+class Student{
+
     private $connection;
 
     public function __construct()
     {
-      $database = new Database();
-      $this->connection = $database->connect();
+       $database = new Database();
+       $this->connection = $database->connect();
     }
 
-    public function getAll(){
-      return $this->connection->query("SELECT * FROM students");
+    //CRUD - CREATE STUDENT, READ all student , UPDATE student  , DELETE student.  
+
+
+    //function to read all student from the database
+    public function getAllStudent(){
+        return $this->connection->query("SELECT * FROM students");
     }
 
+
+    //get student by ID
     public function getById($idno){
-      return $this->connection->query("SELECT * FROM students WHERE idno = $idno");
+        return $this->connection->query("SELECT * FROM students WHERE idno = $idno");
     }
     
-    public function add($idno, $firstname, $lastname, $year_level, $course){
-      $stmt = $this->connection->prepare("INSERT INTO students(idno,firstname,lastname,year_level,course) VALUES(?,?,?,?,?)" );
-      $stmt->bind_param("sssss",$idno, $firstname, $lastname, $year_level, $course );
-      return $stmt->execute();  
-
+    //function to create student
+    public function add($idno,$lastname,$firstname,$year_level,$course){
+        $stmt = $this->connection->prepare("INSERT INTO students(idno,lastname,firstname,year_level,course)  VALUES(?,?,?,?,?)"); 
+        $stmt->bind_param("sssss",$idno,$lastname,$firstname,$year_level,$course);
+        return $stmt->execute();
     }
 
+    //update student
+    public function update($idno,$lastname,$firstname,$year_level,$course){
+        $stmt = $this->connection->prepare("UPDATE students SET lastname = ?, firstname = ? , year_level = ? , course = ? WHERE idno = $idno"); 
+        $stmt->bind_param("ssss",$lastname,$firstname,$year_level,$course);
+        return $stmt->execute();
+    }
+
+    //delete student
     public function delete($idno){
-      return $this->connection->query("DELETE FROM students WHERE idno = $idno");
+        return $this->connection->query("DELETE FROM students WHERE idno = $idno");
     }
-
-    public function update($idno, $firstname, $lastname, $year_level, $course){
-      $stmt = $this->connection->prepare("UPDATE students SET lastname = ? , firstname = ? , year_level = ?, course = ? WHERE idno = $idno");
-      $stmt->bind_param("ssss",$firstname, $lastname, $year_level, $course);
-       return $stmt->execute();  
-
-    }
-
- }
+}
 
 ?>
